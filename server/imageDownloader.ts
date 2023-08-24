e89204ce099329647a4ccadf2ca9ddcd83ceeb13
femogiga <femogiga@yahoo.com>
2023-08-24 16:39:38 +0100

    ready to deploy

--- server/imageDownloader.ts
+++ server/imageDownloader.ts
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const https = require('https');
 const path = require('path')
+const download = require('image-downloader')
+
 
 const imageDownloader = (label, url) => {
     const fileExtension = path.extname(url)
     const filePath = `${label}${fileExtension}`
     const file = fs.createWriteStream(filePath);
+
 
     https.get(url, response => {
         response.pipe(file);
@@ -20,4 +23,16 @@ const imageDownloader = (label, url) => {
     });
 };
 
-module.exports = { imageDownloader };
+
+function downloadImage(url, label) {
+    const destinationPath = path.join(__dirname, 'photos')
+    console.log(__dirname)
+    return download.image({
+        url,
+        dest: `${destinationPath}/${label}.jpeg`
+    })
+
+
+}
+
+module.exports = { imageDownloader, downloadImage };
--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,7 @@
 const { imageDownloader } = require('./imageDownloader.ts')
+const { downloadImage } = require('./imageDownloader.ts')
+const path = require('path')
+
 
 
 const express = require('express')
@@ -10,12 +13,16 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
+app.use(express.static(path.join(__dirname, "/photos")))
 
 let urlString = 'https://images.pexels.com/photos/9571276/pexels-photo-9571276.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'
 
-imageDownloader('arsenal', urlString)
+// imageDownloader('arsenal', urlString)
+downloadImage(urlString, 'arsenal')
 
 
+const urlString2 = "https://images.pexels.com/photos/11857612/pexels-photo-11857612.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+downloadImage(urlString2, 'Chelsea')
 
 app.get('/', (req, res) => {
     res.send('Welcome to my app')
